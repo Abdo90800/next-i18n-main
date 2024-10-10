2acca9d42b806edb3d484b199179bc3b7ea9571a
Abdo90800 <zezocr7missi@gmail.com>
2024-10-10 18:28:25 +0300

    2

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,11 +1,10 @@
-import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
 import Header from "../../component/Header";
-import './globals.css';
 
 export default async function LocaleLayout({
   children,
-  params: { locale }
+  params: { locale },
 }: {
   children: React.ReactNode;
   params: { locale: string };
@@ -14,13 +13,13 @@ export default async function LocaleLayout({
   const messages = await getMessages();
 
   // Determine the text direction based on the locale
-  const direction = locale === 'ar' ? 'rtl' : 'ltr';
+  const direction = locale === "ar" ? "rtl" : "ltr";
 
   return (
     <html lang={locale} dir={direction}>
       <body>
         <NextIntlClientProvider messages={messages}>
-        <Header />
+          <Header />
           {children}
         </NextIntlClientProvider>
       </body>
--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,9 +1,23 @@
 "use client";
-import React, { useState } from "react";
+import { styled, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
 import CssBaseline from "@mui/material/CssBaseline";
-import NavBar from "./NavBar";
-import SideBar from "./SideBar";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useLocale } from "next-intl";
 import { RiDashboard2Line } from "react-icons/ri";
 import { GrCatalogOption } from "react-icons/gr";
 import { MdMonetizationOn } from "react-icons/md";
@@ -16,12 +30,136 @@ import { MdOutlinePriceChange } from "react-icons/md";
 import { FaRegFolderOpen } from "react-icons/fa6";
 import { FaSignalMessenger } from "react-icons/fa6";
 import { TbCashRegister } from "react-icons/tb";
+import Stack from "@mui/material/Stack";
+import { IoMdNotificationsOutline } from "react-icons/io";
 import { BsArrowLeftRight } from "react-icons/bs";
-import { useTranslations } from "next-intl";
-import Stack from '@mui/material/Stack';
+import { BsCommand } from "react-icons/bs";
+import { IoMoonOutline } from "react-icons/io5";
+import LocalSwitcher from "./LocalSwitcher";
+
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
+
+const menuItems = [
+  { text: "Home", icon: <RiDashboard2Line color="#fff" />, link: "/home" },
+  { text: "Catalog", icon: <GrCatalogOption color="#fff" />, link: "/catalog" },
+  { text: "Sales", icon: <MdMonetizationOn color="#fff" />, link: "/sales" },
+  { text: "Clients", icon: <IoPersonOutline color="#fff" />, link: "/clients" },
+  { text: "Shopping", icon: <MdShoppingBag color="#fff" />, link: "/shopping" },
+  {
+    text: "Content Management",
+    icon: <LuPencilLine color="#fff" />,
+    link: "/content-management",
+  },
+];
+
+const additionalMenuItems = [
+  {
+    text: "Discount",
+    icon: <RiDiscountPercentLine color="#fff" />,
+    link: "/discount",
+  },
+  { text: "Stores", icon: <LiaStoreSolid color="#fff" />, link: "/stores" },
+  {
+    text: "Prices",
+    icon: <MdOutlinePriceChange color="#fff" />,
+    link: "/prices",
+  },
+  {
+    text: "Principles",
+    icon: <FaRegFolderOpen color="#fff" />,
+    link: "/principles",
+  },
+  {
+    text: "Rating and Comments",
+    icon: <FaSignalMessenger color="#fff" />,
+    link: "/rating-comments",
+  },
+  {
+    text: "Basic Payment Methods",
+    icon: <TbCashRegister color="#fff" />,
+    link: "/payment-methods",
+  },
+  {
+    text: "Returns",
+    icon: <BsArrowLeftRight color="#fff" />,
+    link: "/returns",
+  },
+];
+
 export default function Header() {
-  const t = useTranslations('HomePage'); // استخدام الترجمة الخاصة بـ HomePage
+  const theme = useTheme();
   const [open, setOpen] = useState(false);
+  const locale = useLocale();
+  const router = useRouter();
+  const direction = locale === "ar" ? "rtl" : "ltr";
+
+  useEffect(() => {
+    document.documentElement.dir = direction;
+  }, [direction]);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -31,38 +169,117 @@ export default function Header() {
     setOpen(false);
   };
 
-  const arr = [
-    { text: t('home'), icon: <RiDashboard2Line color="#fff" />, link: "/home" },
-    { text: t('catalog'), icon: <GrCatalogOption color="#fff" />, link: "/home" },
-    { text: t('sales'), icon: <MdMonetizationOn color="#fff" />, link: "/home" },
-    { text: t('clients'), icon: <IoPersonOutline color="#fff" />, link: "/home" },
-    { text: t('shopping'), icon: <MdShoppingBag color="#fff" />, link: "/home" },
-    {
-      text: t('contentManagement'),
-      icon: <LuPencilLine color="#fff" />,
-      link: "/home",
-    },
-  ];
-
-  const arr2 = [
-    { text: t('discount'), icon: <RiDiscountPercentLine color="#fff" />, link: "/home" },
-    { text: t('stores'), icon: <LiaStoreSolid color="#fff" />, link: "/home" },
-    { text: t('prices'), icon: <MdOutlinePriceChange color="#fff" />, link: "/home" },
-    { text: t('principles'), icon: <FaRegFolderOpen color="#fff" />, link: "/home" },
-    { text: t('ratingAndComments'), icon: <FaSignalMessenger color="#fff" />, link: "/home" },
-    { text: t('basicPaymentMethods'), icon: <TbCashRegister color="#fff" />, link: "/home" },
-    { text: t('returns'), icon: <BsArrowLeftRight color="#fff" />, link: "/home" },
-  ];
-
   return (
-    <Stack sx={{ display: "flex" }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <NavBar open={open} handleDrawerOpen={handleDrawerOpen} />
-      <SideBar
+      <AppBar position="fixed" open={open} sx={{ background: "#fff" }}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: "none" }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Stack direction={"row"}>
+            <LocalSwitcher />
+            <IconButton aria-label="notifications">
+              <IoMdNotificationsOutline color="#000" />
+            </IconButton>
+            <IconButton aria-label="commands">
+              <BsCommand color="#000" />
+            </IconButton>
+            <IconButton aria-label="dark mode">
+              <IoMoonOutline color="#000" />
+            </IconButton>
+          </Stack>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        anchor={direction}
+        variant="permanent"
         open={open}
-        handleDrawerClose={handleDrawerClose}
-        items={[...arr, ...arr2]}
-      />
-    </Stack>
+        sx={{
+          "& .MuiDrawer-paper": {
+            backgroundColor: "#344170",
+            color: "#fff",
+          },
+        }}
+      >
+        <DrawerHeader dir={direction}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {menuItems.map((item) => (
+            <ListItem key={item.link} disablePadding sx={{ display: "block" }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  px: 2.5,
+                  ...(open
+                    ? { justifyContent: "initial" }
+                    : { justifyContent: "center" }),
+                }}
+                onClick={() => router.push(item.link)}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    justifyContent: "center",
+                    ...(open ? { mr: 3 } : { mr: "auto" }),
+                  }}
+                >
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText primary={item.text} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {additionalMenuItems.map((item) => (
+            <ListItem key={item.link} disablePadding sx={{ display: "block" }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  px: 2.5,
+                  ...(open
+                    ? { justifyContent: "initial" }
+                    : { justifyContent: "center" }),
+                }}
+                onClick={() => router.push(item.link)}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    justifyContent: "center",
+                    ...(open ? { mr: 3 } : { mr: "auto" }),
+                  }}
+                >
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText primary={item.text} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
   );
 }
